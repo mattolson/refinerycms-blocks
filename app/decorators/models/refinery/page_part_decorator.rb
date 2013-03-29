9038b80914a1fee6d93bcdcdc4687c8e3f98f6c4
Matt Olson <matt@mattolson.com>
2013-03-29 11:16:28 -0700

    Don't query the database with emtpy block list

--- app/decorators/models/refinery/page_part_decorator.rb
+++ app/decorators/models/refinery/page_part_decorator.rb
@@ -7,13 +7,14 @@
     # Get list of block references
     block_ids = body.scan(/\{\{\s*(\S+)\s*\}\}/).flatten.uniq
 
-    # Retrieve content from database
-    blocks = ::Refinery::Blocks::Block.select("permatitle, content").where(:permatitle => block_ids)
-
     # Make substitutions
     @expanded_body = body
-    blocks.each do |block|
-      @expanded_body.gsub! /\{\{\s*#{block.permatitle}\s*\}\}/, block.content
+    unless block_ids.empty?
+      # Retrieve content from database
+      blocks = ::Refinery::Blocks::Block.select("permatitle, content").where(:permatitle => block_ids)
+      blocks.each do |block|
+        @expanded_body.gsub! /\{\{\s*#{block.permatitle}\s*\}\}/, block.content
+      end
     end
 
     @expanded_body
